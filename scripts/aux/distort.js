09abf69be7f5503299c291162882af6daec694ab
Georg Fischer <snorpey@gmail.com>
2013-07-19 22:35:09 +0200

    fix distortion to work correctly

--- scripts/aux/distort.js
+++ scripts/aux/distort.js
@@ -6,125 +6,73 @@ define(
 		var canvas = document.createElement( 'canvas' );
 		var ctx = canvas.getContext( '2d' );
 
-		var tmp_canvas = document.createElement( 'canvas' );
-		var tmp_ctx = tmp_canvas.getContext( '2d' );
-
-		var res_canvas = document.createElement( 'canvas' );
-		var res_ctx = res_canvas.getContext( '2d' );
-
-		var amount;
-		var grid_size;
-		var detail;
-
 		var direction = { x: 0, y: 1 };
 
 		var i, j;
 		var len, len_2;
 
-		var cell_count = 0;
-		var cells_created = 0;
-
 		var done;
 
-
-		function applyFilter( image_data, input, callback )
+		function applyFilter( image, points, grid_size, callback )
 		{
-			amount      = input.amount;
-			grid_size   = input.grid_size;
-			detail      = 100 - input.detail;
-			direction.x = input.horizontal;
-			direction.y = input.vertical;
-			done        = callback;
+			done = callback;
 
-			res_ctx.clearRect( 0, 0, res_canvas.width, res_canvas.height );
-			tmp_ctx.clearRect( 0, 0, tmp_canvas.width, tmp_canvas.height );
 			ctx.clearRect( 0, 0, canvas.width, canvas.height );
-
-			canvas_helper.resize( canvas, image_data );
-			canvas_helper.resize( tmp_canvas, image_data );
-			canvas_helper.resize( res_canvas, image_data );
-
-			ctx.putImageData( image_data, 0, 0 );
-
-			process( image_data, amount );
+			canvas_helper.resize( canvas, image );
+			process( image, points, grid_size );
 		}
 
-		function process( image_data, amount )
+		function process( image, points, grid_size )
 		{
-			var width = image_data.width;
-			var height = image_data.height;
-			var grid_points = getGridPoints( image_data, grid_size, grid_size );
-			var distorted_points = getDistortedPoints( grid_points, grid_size, grid_size );
+			len = points.length;
 
-			canvas_helper.resize( tmp_canvas, { width: grid_size, height: grid_size } );
+			for ( i = 0; i < len; i++ )
+			{
+				processPoint( ctx, image, points, points[i], grid_size );
+			}
 
-			distorted_points.forEach( processPoint );
+			done( ctx.getImageData( 0, 0, canvas.width, canvas.height ) );
 		}
 
-		function processPoint( p1, index, distorted_points )
+		function processPoint( ctx, image, points, p1, grid_size )
 		{
-			var p2 = getItemByValue( distorted_points, 'row', p1.row + 1, 'column', p1.column );
-			var p3 = getItemByValue( distorted_points, 'row', p1.row,     'column', p1.column + 1 );
-			var p4 = getItemByValue( distorted_points, 'row', p1.row + 1, 'column', p1.column + 1 );
+			var p3 = getItemByValue( points, 'row', p1.row + 1, 'column', p1.column );
+			var p2 = getItemByValue( points, 'row', p1.row,     'column', p1.column + 1 );
+			var p4 = getItemByValue( points, 'row', p1.row + 1, 'column', p1.column + 1 );
 
 			if ( p1 && p2 && p3 && p4 )
 			{
-				tmp_ctx.putImageData( p1.image_data, 0, 0 );
+				var xm = getLinearSolution( 0, 0, p1.x_end, grid_size, 0, p2.x_end, 0, grid_size, p3.x_end );
+				var ym = getLinearSolution( 0, 0, p1.y_end, grid_size, 0, p2.y_end, 0, grid_size, p3.y_end );
+				var xn = getLinearSolution( grid_size, grid_size, p4.x_end, grid_size, 0, p2.x_end, 0, grid_size, p3.x_end );
+				var yn = getLinearSolution( grid_size, grid_size, p4.y_end, grid_size, 0, p2.y_end, 0, grid_size, p3.y_end );
 
-				var img = new Image();
+				ctx.save();
+				ctx.setTransform( xm[0], ym[0], xm[1], ym[1], xm[2], ym[2] );
+				ctx.beginPath();
+				ctx.moveTo( 0, 0 );
+				ctx.lineTo( grid_size, 0 );
+				ctx.lineTo( 0, grid_size );
+				ctx.lineTo( 0, 0 );
+				ctx.closePath();
+				ctx.fill();
+				ctx.clip();
+				ctx.drawImage( image, p1.x, p1.y, grid_size, grid_size, 0, 0, grid_size, grid_size );
+				ctx.restore();
 
-				img.onload = function()
-				{
-					drawCell( res_ctx, img, p1, p2, p3, p4 );
-
-					cells_created++;
-
-					if ( cell_count === cells_created )
-					{
-						done( res_ctx.getImageData( 0, 0, canvas.width, canvas.height ) );
-					}
-				};
-
-				img.src = tmp_canvas.toDataURL( 'image/png' );
-
-				cell_count++;
+				ctx.save();
+				ctx.setTransform( xn[0], yn[0], xn[1], yn[1], xn[2], yn[2] );
+				ctx.beginPath();
+				ctx.moveTo( grid_size, grid_size );
+				ctx.lineTo( grid_size, 0 );
+				ctx.lineTo( 0, grid_size );
+				ctx.lineTo( grid_size, grid_size );
+				ctx.closePath();
+				ctx.fill();
+				ctx.clip();
+				ctx.drawImage( image, p1.x, p1.y, grid_size, grid_size, 0, 0, grid_size, grid_size );
+				ctx.restore();
 			}
-		}
-
-		// by @migurski
-		// http://mike.teczno.com/notes/canvas-warp.html
-		function drawCell( ctx, image, p1, p2, p3, p4 )
-		{
-			var xm = getLinearSolution( 0, 0, p1.end_x, image.width, 0, p2.end_x, 0, image.height, p3.end_x );
-			var ym = getLinearSolution( 0, 0, p1.end_y, image.width, 0, p2.end_y, 0, image.height, p3.end_y );
-			var xn = getLinearSolution( image.width, image.height, p4.end_x, image.width, 0, p2.end_x, 0, image.height, p3.end_x );
-			var yn = getLinearSolution( image.width, image.height, p4.end_y, image.width, 0, p2.end_y, 0, image.height, p3.end_y );
-
-			ctx.save();
-			ctx.setTransform( xm[0], ym[0], xm[1], ym[1], xm[2], ym[2] );
-			ctx.beginPath();
-			ctx.moveTo( 0, 0 );
-			ctx.lineTo( image.width, 0 );
-			ctx.lineTo( 0, image.height );
-			ctx.lineTo( 0, 0 );
-			ctx.closePath();
-			ctx.fill();
-			ctx.clip();
-			ctx.drawImage( image, 0, 0, image.width, image.height );
-			ctx.restore();
-
-			ctx.save();
-			ctx.setTransform( xn[0], yn[0], xn[1], yn[1], xn[2], yn[2] );
-			ctx.beginPath();
-			ctx.moveTo( image.width, image.height );
-			ctx.lineTo( image.width, 0 );
-			ctx.lineTo( 0, image.height );
-			ctx.lineTo( image.width, image.height );
-			ctx.closePath();
-			ctx.fill();
-			ctx.clip();
-			ctx.drawImage( image, 0, 0, image.width, image.height );
-			ctx.restore();
 		}
 
 		function getLinearSolution( r1, s1, t1, r2, s2, t2, r3, s3, t3 )
@@ -189,36 +137,6 @@ define(
 			grid_points.columns = column;
 
 			return grid_points;
-		}
-
-		function getDistortedPoints( grid_points, tile_width, tile_height )
-		{
-			var tile_image_data;
-			var cell_contrast;
-			var key;
-			var distortion;
-			var tile_points = [ ];
-
-			len = grid_points.length;
-
-			for ( i = 0; i < len; i++ )
-			{
-				tile_image_data = ctx.getImageData( grid_points[i].x, grid_points[i].y, tile_width, tile_height );
-				cell_contrast   = getImageDataContrast( tile_image_data, detail );
-				distortion      = amount * cell_contrast;
-
-				grid_points[i].image_data = tile_image_data;
-				grid_points[i].end_x = parseInt( grid_points[i].x + ( direction.x * distortion ), 10 );
-				grid_points[i].end_y = parseInt( grid_points[i].y + ( direction.y * distortion ), 10 );
-			}
-
-			return grid_points;
-		}
-
-		function drawBackground( canvas, ctx, color )
-		{
-			ctx.fillStyle = color;
-			ctx.fillRect( 0, 0, canvas.width, canvas.height );
 		}
 
 		function getItemByValue( items, key, value, key_2, value_2 )
