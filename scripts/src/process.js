9fd45bc08c88ca63d033d3292efa7299e9dee7bd
Georg Fischer <snorpey@gmail.com>
2013-07-19 21:47:08 +0200

    distortion almost working. all tiles are rotated by 90deg for some reason, though

--- scripts/src/process.js
+++ scripts/src/process.js
@@ -3,24 +3,22 @@ define(
 	[ 'aux/distort', 'aux/canvas', 'lib/raf' ],
 	function( distort, canvas_helper )
 	{
-		var tmp_canvas = document.createElement( 'canvas' );
-		var tmp_ctx = tmp_canvas.getContext( '2d' );
-
-		var canvas = document.getElementById( 'canvas' );
+		var canvas = document.getElementById( 'image-canvas' );
 		var ctx = canvas.getContext( '2d' );
 
 		var is_processing = false;
 		var values;
 		var image;
 		var signals;
-		var image_data;
 		var canvas_size;
+		var points;
 
 		function init( shared )
 		{
 			signals = shared.signals;
 
 			signals['image-loaded'].add( generate );
+			signals['points-updated'].add( pointsUpdated );
 			signals['control-updated'].add( controlsUpdated );
 			signals['saved'].add( exportData );
 		}
@@ -28,6 +26,13 @@ define(
 		function controlsUpdated( new_values )
 		{
 			values = getAdjustedValues( new_values );
+
+			update();
+		}
+
+		function pointsUpdated( new_points )
+		{
+			points = new_points;
 
 			update();
 		}
@@ -68,21 +73,17 @@ define(
 		{
 			is_processing = true;
 
-			canvas_helper.resize( tmp_canvas, img );
 			canvas_helper.resize( canvas, img );
 
-			tmp_ctx.drawImage( img, 0, 0 );
-
-			image_data = tmp_ctx.getImageData( 0, 0, tmp_canvas.width, tmp_canvas.height );
-
-			distort( image_data, values, draw );
+			if ( image && points && values )
+			{
+				distort( image, points, values.grid_size, draw );
+			}
 		}
 
 		function draw( image_data )
 		{
 			ctx.clearRect( 0, 0, canvas.width, canvas.height );
-			tmp_ctx.clearRect( 0, 0, tmp_canvas.width, tmp_canvas.height );
-
 			canvas_helper.resize( canvas, image_data );
 			ctx.putImageData( image_data, 0, 0 );
 
@@ -101,18 +102,7 @@ define(
 
 			for ( var key in new_values )
 			{
-				if (
-					key === 'horizontal' ||
-					key === 'vertical'
-				)
-				{
-					result[key] = parseFloat( new_values[key] );
-				}
-
-				else
-				{
-					result[key] = parseInt( new_values[key], 10 );
-				}
+				result[key] = parseInt( new_values[key], 10 );
 			}
 
 			key = null;
